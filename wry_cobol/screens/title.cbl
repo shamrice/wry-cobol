3572f53b12348cc8f27c9fa489bef6ac32b11a77
Erik E <shamrice@gmail.com>
2018-12-20 18:25:48 -0500

    Title screen
    in the works

---
+++ wry_cobol/screens/title.cbl
@@ -0,0 +1,61 @@
+       01  TITLE-SCREEN.
+           05 VALUE '                    Nukem Enterprises'.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 COL 1,
+       VALUE '####              *####   #########      ###      ###'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '*####            *####   *###*****###    *###   *###'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE ' *####          *####    *###    *###     *### *###'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '  *####        *####     *#########        *#####'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '   *####  *#  *####      *###    *###       *###'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '    *####*###*####       *###     *###     *###'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '     *#####*#####        *###      *###   *###'.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '      *###  *###         ***       ***    ***'.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 COL 1,
+           VALUE '                 C    O    B    O    L'.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 COL 1
+           VALUE '                 A TEXTED BASED COMEDY'.
+           05 LINE + 1.
+           05 COL 1
+           VALUE '                       Ver. 0.002'.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 COL 1
+           VALUE '          PRESS ENTER KEY TO GO TO THE MENU'.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+      *> 05 VALUE 'WRY (R) is copyright of Nukem Enterprises
+      *> and Sim Creations Inc.'.
+           05 LINE + 1.
+           05 COL 1
+           VALUE '                Nukem Enterprises  2018'.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 LINE + 1.
+           05 COL 1, LINE + 1.
+           05 RESPONSE-INPUT PIC X TO WS-TEST-VAR.
--- wry_cobol/wry-cobol.cbl
+++ wry_cobol/wry-cobol.cbl
@@ -1,9 +1,9 @@
-      ******************************************************************
-      * Author: Erik Eriksen
-      * Date: 12/16/2018
-      * Purpose: Version of Wry written in COBOL
-      * Tectonics: cobc
-      ******************************************************************
+      *>*****************************************************************
+      *> Author: Erik Eriksen
+      *> Date: 12/16/2018
+      *> Purpose: Version of Wry written in COBOL
+      *> Tectonics: cobc
+      *>*****************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. WRY-COBOL.
 
@@ -14,10 +14,13 @@
                SELECT FD-STORY-FILE
                    ASSIGN TO '../data/story.dat'
                    ORGANIZATION IS LINE SEQUENTIAL.
-      *             ACCESS IS SEQUENTIAL.
 
                SELECT FD-STORY-TEXT-FILE
                    ASSIGN TO "../data/story-text.dat"
+                   ORGANIZATION IS LINE SEQUENTIAL.
+
+               SELECT FD-TITLE-FILE
+                   ASSIGN TO '../data/title.dat'
                    ORGANIZATION IS LINE SEQUENTIAL.
 
        DATA DIVISION.
@@ -26,6 +29,7 @@
 
        FD  FD-STORY-FILE.
        01  FD-STORY-RECORD.
+           88 EOF-STORY                        VALUE HIGH-VALUES.
            05 STORY-ID                         PIC 9(4).
            05 CORRECT-STORY-ID                 PIC 9(4).
            05 CORRECT-CHOICE-ID                PIC 9(1).
@@ -34,13 +38,21 @@
 
        FD  FD-STORY-TEXT-FILE.
        01  FD-STORY-TEXT-RECORD.
-           05 STORY-TEXT-ID                         PIC 9(4).
+           88 EOF-STORY-TEXT                   VALUE HIGH-VALUES.
+           05 STORY-TEXT-ID                    PIC 9(4).
            05 STORY-TEXT                       PIC X(255).
+
+       FD  FD-TITLE-FILE.
+       01  FD-TITLE-TEXT.
+           88 EOF-TITLE                        VALUE HIGH-VALUES.
+           05 TITLE-TEXT                       PIC X(120).
 
        WORKING-STORAGE SECTION.
 
        77  WS-VERSION-NUM                      PIC X(5) VALUE "0.001".
        77  WS-CURRENT-RECORD                   PIC 9(4) VALUE 0000.
+
+       77  WS-TEST-VAR                         PIC X.
 
        01  WS-EOF-SW                           PIC X(1) VALUE 'N'.
            88 EOF-SW                           VALUE 'Y'.
@@ -57,21 +69,35 @@
            05 WS-STORY-TEXT-ID                 PIC 9(4).
            05 WS-STORY-TEXT                    PIC X(255).
 
+       01  WS-STORY-TEXT-RECORD-FOUND          PIC A(1) VALUE 'N'.
+           88 RECORD-FOUND                     VALUE 'Y'.
+           88 RECORD-NOT-FOUND                 VALUE 'N'.
+
+
+       SCREEN SECTION.
+       COPY 'screens/title.cbl'.
+
+
        PROCEDURE DIVISION.
 
        000-MAIN-PROCEDURE.
+           DISPLAY TITLE-SCREEN
+           ACCEPT TITLE-SCREEN
 
-           DISPLAY 'READING STORY FILE...'
-           PERFORM 100-READ-STORY
+      *>     PERFORM 300-READ-TITLE
+      *>     ACCEPT WS-VERSION-NUM FROM CONSOLE
+
+      *>     DISPLAY 'READING STORY FILE...'
+      *>     PERFORM 100-READ-STORY
 
            STOP RUN.
 
        100-READ-STORY.
 
            OPEN INPUT FD-STORY-FILE
-               PERFORM UNTIL EOF-SW
+               PERFORM UNTIL EOF-STORY
                    READ FD-STORY-FILE INTO WS-STORY-RECORD
-                       AT END MOVE 'Y' TO WS-EOF-SW
+                       AT END SET EOF-STORY TO TRUE
                        NOT AT END
                            DISPLAY WS-STORY-ID
                            DISPLAY WS-CORRECT-STORY-ID
@@ -88,16 +114,33 @@
 
        200-READ-STORY-TEXT.
            OPEN INPUT FD-STORY-TEXT-FILE
-               PERFORM UNTIL EOF-SW
+               DISPLAY 'Reading story data'
+               PERFORM UNTIL EOF-SW OR RECORD-FOUND
+                   DISPLAY 'Not at eof so reading story...'
                    READ FD-STORY-TEXT-FILE INTO WS-STORY-TEXT-RECORD
                        AT END MOVE 'Y' TO WS-EOF-SW
                        NOT AT END
                            IF WS-STORY-TEXT-ID = WS-STORY-ID
                                DISPLAY WS-STORY-TEXT
+                               MOVE 'Y' TO WS-STORY-TEXT-RECORD-FOUND
                            END-IF
                    END-READ
                END-PERFORM
+               DISPLAY 'Done story data perform.. clsoing file'
            CLOSE FD-STORY-TEXT-FILE
-           MOVE 'N' TO WS-EOF-SW.
+           MOVE 'N' TO WS-EOF-SW
+           MOVE 'N' TO WS-STORY-TEXT-RECORD-FOUND.
+
+
+       300-READ-TITLE.
+           OPEN INPUT FD-TITLE-FILE
+               PERFORM UNTIL EOF-TITLE
+                   READ FD-TITLE-FILE
+                       AT END SET EOF-TITLE TO TRUE
+                       NOT AT END
+                           DISPLAY TITLE-TEXT
+                   END-READ
+               END-PERFORM
+           CLOSE FD-TITLE-FILE.
 
        END PROGRAM WRY-COBOL.
